9342424fad8184aab4d8b68e9750c76777472bdc
pxj-1104 <73725948+pxj-1104@users.noreply.github.com>
2025-10-16 18:50:56 +0800

    Update dataload.py

--- dataprocess_train/dataload.py
+++ dataprocess_train/dataload.py
@@ -1,3 +1,5 @@
+# csv data to npy
+
 import os
 import numpy as np
 import pandas as pd
@@ -7,16 +9,16 @@ from sklearn.preprocessing import LabelEncoder, StandardScaler,RobustScaler
 base_dir = '../data/csv_data/testdata'
 save_dir = '../data/dataset/group-data/separate-4-120'
 
-
+# This value should be precomputed for robust scaling, and will generally work better with large datasets.
 percentiles = { #cache-references,cache-misses,L1-dcache-loads,L1-dcache-load-misses,L1-icache-load-misses,LLC-loads,LLC-load-misses,LLC-store-misses
-    0: {'1st': 250965.0, '5th': 363882.0,'25th': 902376.0, '50th': 2957030.5 ,'75th': 6660350.0 ,'95th': 14434327.0, '99th': 22337618.9,  },
-    1: {'1st': 923.0, '5th': 1486.0,'25th': 4924.0, '50th': 130003.0 ,'75th': 606428.0 ,'95th': 3592487.1, '99th': 6176059.6,  },
-    2: {'1st': 487484.0, '5th': 1824049.9,'25th': 20210061.5, '50th': 37898381.5, '75th': 61534066.8 ,'95th': 107391350.0, '99th': 143820937.4,  },
-    3: {'1st': 37056.0, '5th': 55110.0,'25th': 320507.0, '50th': 1304914.0, '75th': 2985272.5 ,'95th': 6459636.2, '99th': 22905894.5,  },
-    4: {'1st': 116896.0, '5th': 177093.0,'25th': 340360.0, '50th': 1277704.5, '75th': 3783308.2 ,'95th': 8714825.2, '99th': 12625176.2,  },
-    5: {'1st': 13727.0, '5th': 21781.0,'25th': 89136.0, '50th': 254444.0, '75th': 646203.0 ,'95th': 1617420.0, '99th': 3697958.4,  },
-    6: {'1st': 162.0, '5th': 320.0,'25th': 806.0, '50th': 8044.0, '75th': 68963.0 ,'95th': 349035.0, '99th': 1022372.0,  },
-    7: {'1st': 219.0, '5th': 514.0,'25th': 1020.0, '50th': 6513.0, '75th': 81318.0 ,'95th': 770937.1, '99th': 2505278.9,  },
+    # 0: {'1st': xxxx, '5th': xxxx,'25th': xxxx, '50th': xxxx ,'75th': xxxx ,'95th': xxxx, '99th': xxxx,  },
+    # 0: {'1st': xxxx, '5th': xxxx,'25th': xxxx, '50th': xxxx ,'75th': xxxx ,'95th': xxxx, '99th': xxxx,  },
+    # 0: {'1st': xxxx, '5th': xxxx,'25th': xxxx, '50th': xxxx ,'75th': xxxx ,'95th': xxxx, '99th': xxxx,  },
+    # 0: {'1st': xxxx, '5th': xxxx,'25th': xxxx, '50th': xxxx ,'75th': xxxx ,'95th': xxxx, '99th': xxxx,  },
+    # 0: {'1st': xxxx, '5th': xxxx,'25th': xxxx, '50th': xxxx ,'75th': xxxx ,'95th': xxxx, '99th': xxxx,  },
+    # 0: {'1st': xxxx, '5th': xxxx,'25th': xxxx, '50th': xxxx ,'75th': xxxx ,'95th': xxxx, '99th': xxxx,  },
+    # 0: {'1st': xxxx, '5th': xxxx,'25th': xxxx, '50th': xxxx ,'75th': xxxx ,'95th': xxxx, '99th': xxxx,  },
+    # 0: {'1st': xxxx, '5th': xxxx,'25th': xxxx, '50th': xxxx ,'75th': xxxx ,'95th': xxxx, '99th': xxxx,  },
 }
 
 
@@ -217,5 +219,6 @@ np.save(os.path.join(save_dir, 'medium_test_data.npy'), medium_test_data)
 np.save(os.path.join(save_dir, 'medium_test_labels.npy'), medium_test_labels)
 np.save(os.path.join(save_dir, 'low_test_data.npy'), low_test_data)
 np.save(os.path.join(save_dir, 'low_test_labels.npy'), low_test_labels)
+
 
 
